a40f37e9f7f2da125796d0751f9e7b93a9e3f67b
Leamida <leoanangmh@gmail.com>
2024-10-23 10:25:26 +0700

    non mandatory po_do

--- app/Http/Requests/WeightOutRequest.php
+++ app/Http/Requests/WeightOutRequest.php
@@ -27,7 +27,7 @@ class WeightOutRequest extends FormRequest
             'weight_out' => 'required|max:10',
             'vehicle_no' => 'required|string',
             'remark' => 'nullable|string|max:255',
-            'po_do' => 'nullable|string|max:255|required_if:weighing_type,fg',
+            'po_do' => 'nullable|string|max:255',
             'weighing_type' => 'required|in:rm,fg',
         ];
     }
