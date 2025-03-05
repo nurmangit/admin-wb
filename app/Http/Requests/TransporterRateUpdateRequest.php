ca641629cc994bb10b38f3c636b8b04436fae6fa
Leamida <leoanangmh@gmail.com>
2025-03-05 11:21:53 +0700

    fix

---
+++ app/Http/Requests/TransporterRateUpdateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class TransporterRateStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:255',
+            'area_uuid' => 'required|uuid',
+            'vehicle_type_uuid' => 'required|uuid',
+            'charge' => 'nullable|numeric|min:0',
+            'start_date' => 'required|date_format:Y-m-d',
+            'end_date' => 'required|date_format:Y-m-d|after:start_date'
+        ];
+    }
+}
